aaac950a99144b1b8468b2d252b313e18352327b
JoshuaRaybould <joshua.raybould@outlook.com>
2025-09-13 15:58:53 +0100

    improve selection of hyperparameters

--- backend/train.py
+++ backend/train.py
@@ -1,34 +1,35 @@
 import pandas as pd
 
-from surprise import Dataset, SVD, Reader
-from surprise.model_selection import cross_validate
-from sklearn.model_selection import train_test_split
+from surprise import Dataset, SVD, Reader, accuracy
+from surprise.model_selection import cross_validate, train_test_split
+from sklearn.model_selection import train_test_split as sk_train_test_split
 
 # Load our dataset
 df = pd.read_csv("../movieData/ratings.csv")
-# print(df.head())
-trainset, testset = train_test_split(df, test_size=0.10)
+
+# Split into a training set and test set
+trainset, testset = sk_train_test_split(df, test_size=0.10)
 
 # Hyperparameter options
 n_factors = [20, 25, 30, 45]
 n_epochs = [15, 25]
 
-# Get the data ready for reading
+# Prepare the data for reading and read it
 reader = Reader(rating_scale=(1,5))
-movieData = Dataset.load_from_df(trainset[["userId", "movieId", "rating"]], reader)
-
-# Split into a training set and test set
+trainData = Dataset.load_from_df(trainset[["userId", "movieId", "rating"]], reader)
+testData = Dataset.load_from_df(testset[["userId", "movieId", "rating"]], reader)
 
 folds = 5
 bestRMSE = float('infinity')
 chosenFactor = -1
 chosenEpoch = -1
 
+# Search for the best hyperparameters
 for factorChoice in n_factors:
     for epochChoice in n_epochs:
         svd = SVD(n_factors=factorChoice, n_epochs=epochChoice)
         print("Current iteration we are using " + str(factorChoice) + " factors and " + str(epochChoice) + " epochs")
-        res = cross_validate(svd, movieData, measures=['RMSE'], cv=folds)
+        res = cross_validate(svd, trainData, measures=['RMSE'], cv=folds)
         curMeanRMSE = sum(res['test_rmse'])/folds
         print(curMeanRMSE)
 
@@ -37,9 +38,26 @@ for factorChoice in n_factors:
             chosenFactor = factorChoice
             chosenEpoch = epochChoice
 
-print("We end with")
+# Fit the model with our chosen hyperparameters to the training set
+print("We chose to use " + str(chosenFactor) + " factors and " + str(chosenEpoch) + " epochs")
 print(chosenFactor)
 print(chosenEpoch)
+movieData = Dataset.load_from_df(df[["userId", "movieId", "rating"]], reader)
+
+svd = SVD(n_factors=chosenFactor, n_epochs=chosenEpoch)
+trainData, testData = train_test_split(movieData, test_size=0.10)
+svd.fit(trainData)
+
+# Now measure our performance against the test set
+
+predictions = svd.test(testData)
+accuracy.rmse(predictions)
+
+# Now that we have chosen our hyperparameters, if we are happy with performance we can retrain on the entire dataset
+
+svd.fit(movieData.build_full_trainset())
+
+
 
 # svd.fit(trainset)
 # predictions = svd.test(testset)
